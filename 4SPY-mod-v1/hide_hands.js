7a94bc3619b8a6eede232ecedad2971b37fc5406
Supernaut-exe <itzms24012008@gmail.com>
2025-04-11 15:42:14 +0530

    Delete 4SPY-mod-v1 directory

--- 4SPY-mod-v1/hide_hands.js
+++
@@ -1,6 +0,0 @@
-pc.app.on("Map:Loaded", () => {
-  setTimeout(() => {
-    pc.app.root.findByName("WeaponCenter").findByName("CharacterHandsHolder").setLocalScale(0, 0, 0);
-    pc.settings.hideArms = true
-  }, 2000);
-});
